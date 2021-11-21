33b3b72d30f8fc6d2172ebc5584faa37670fa679
pashwiniis <pashwini684@gmail.com>
2021-11-21 22:57:54 +0530

    Snake and Ladder game played with single player at start position 0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderGame
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("~~~~~~~~~~~~Snake and Ladder game~~~~~~~~~~");
+            SnakeAndLadder game = new SnakeAndLadder();
+            game.Board();
+        }
+    }
+}
